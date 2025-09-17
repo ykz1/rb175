73fa25863c67c880e2c10b38d3629e9a0900cd37
Kyle Zhao <kyle.y.zhao@gmail.com>
2025-09-17 12:14:13 -0400

    Book viewer v1

---
+++ book_viewer/book_viewer.rb
@@ -0,0 +1,17 @@
+require "sinatra"
+require "sinatra/reloader"
+# require "tilt/erubi"
+
+get "/" do
+  @title = "The Adventures of Sherlock Hoooolmes"
+  @contents = File.readlines("./data/toc.txt")
+  erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("./data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
+
+  erb :chapter
+end
