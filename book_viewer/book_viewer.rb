a4bee3aa2893900675a663f64cdec8dee83394c0
Kyle Zhao <kyle.y.zhao@gmail.com>
2025-09-19 06:58:33 -0400

    More Book Viewer

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -6,18 +6,7 @@ require "sinatra/reloader"
 # Before items
 
 before do
-  # Data stuctures:
-  # @contents = [{number: 1, name: 'Chapter 1 name', paragraphs: [{number: 1, text: "she said he said..."}]}, etc...]
-
   @list_of_chapters = File.readlines("./data/toc.txt")
-  # @contents = @list_of_chapters.each_with_index.with_object([]) do |(chapter_name, index), output|
-  #   chapter_number = index + 1
-  #   paragraphs = File.read("./data/chp#{chapter_number}.txt").split("\n\n")
-  #   paragraphs.map!.with_index do |paragraph, index|
-  #     {number: index + 1, text: paragraph}
-  #   end
-  #   output << {number: chapter_number, name: chapter_name, paragraphs: paragraphs}
-  # end
 end
 
 # =================
@@ -33,6 +22,10 @@ helpers do
     text.split("\n\n").map.with_index do |paragraph,index|
       "<p id=\"#{index + 1}\">#{paragraph}</p>"
     end.join
+  end
+
+  def bold_matching(text, term)
+    text.gsub(term, "<strong>#{term}</strong>")
   end
 end
 
@@ -61,46 +54,27 @@ end
 get '/search' do
   search_term = params[:query]
 
-  # unless search_term.nil? || search_term.empty?
-  #   @search_results = @contents.select do |chapter|
-  #     chapter[:paragraphs].map { |paragraph| paragraph[:text] }.join.include?(search_term)
-  #   end
-  #   @search_results.each do |chapter|
-  #     chapter[:paragraphs] = chapter[:paragraphs].select do |paragraph|
-
   unless search_term.nil?
     # @search_results should return an array of chapters with nested array of paragraphs including search term
-    # [{number: 1, name: "some chapter", paragraphs: {1: "once upon a time", 2: "and then he died..."}}]
-    @search_results = 1.upto(@list_of_chapters.size).each_with_object([]) do |chapter_number, results|
-      text = File.read("data/chp#{chapter_number}.txt")
-      paragraphs = text.split("\n\n")
-      matching_paragraphs = paragraphs.each_with_index.with_object({}) do |(text, index), results|
-        if text.include?(search_term)
-          paragraph_number = index + 1
-          results[paragraph_number] = text
-        end
+    # [{number: 1, name: "some chapter", paragraphs: {1: "once upon a time", 99: "and then he died..."}}]
+    
+    # For each chapter(c), check each paragraph(p) and add to output if search term found:
+    @search_results = @list_of_chapters.each_with_index.with_object([]) do |(c_name, c_index), results|
+      c_num = c_index + 1
 
-        chapter_name = # UNFINISHED!!!
-        
-        unless matching_paragraphs.empty?
-          results << {number: chapter_number, chapter_name, matching_paragraphs}
+      paragraphs = File.read("data/chp#{c_num}.txt").split("\n\n")
+      p_matches = paragraphs.each_with_index.with_object({}) do |(p_text, p_index), matches|
+        p_num = p_index + 1
+        if p_text.include?(search_term)
+          matches[p_num.to_s] = p_text
         end
+      end
+
+      unless p_matches.empty?
+        results << {number: c_num, name: c_name, paragraphs: p_matches}
       end
     end
   end
-
-
-
-  unless search_term.nil?
-    @search_results = 1.upto(@list_of_chapters.size).each_with_object([]) do |chapter_number, results|
-      contents = File.read("data/chp#{chapter_number}.txt")
-      if contents.include?(search_term)
-        results << {number: chapter_number, name: @list_of_chapters[chapter_number -1]}
-      end
-    end
-  end
-
-  
 
   erb :search
 end
